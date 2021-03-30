203a6a0f7cb3db665259919c5352ab560f58d1df
smatthew <shanto12@gmail.com>
2021-03-30 15:21:59 +1100

    asdf

--- 2021/reversort.py
+++ 2021/reversort.py
@@ -2,9 +2,11 @@ for t in range(1, int(input())+1):
     N=int(input())
     L = [int(x) for x in input().split()]
     cost=0
+    j = N-1
     for i in range(N):
-        j = L.index(min(L))
-        L[i:j+1].reverse()
+        j = L[i:j+1].index(min(L[i:j+1]))
+        j = j + i
+        L = L[:i] + L[i:j+1][::-1] + L[j+1:]
         cost += j-i + 1
 
     print(f"Case #{t}: {cost}")
