cb2c5cbf3b2ed32b26ffa8b70b93855328cef627
shanto <shanto12@gmail.com>
2022-07-16 15:26:22 +1000

    asdf

---
+++ 2022/puched cards.py
@@ -0,0 +1,13 @@
+for t in range(1, int(input()) + 1):
+    next_lines = input().split()
+    R, C = int(next_lines[0]), int(next_lines[1])
+    L = [int(x) for x in input().split()]
+    cost = 0
+    j = N - 1
+    for i in range(N):
+        j = L[i:j + 1].index(min(L[i:j + 1]))
+        j = j + i
+        L = L[:i] + L[i:j + 1][::-1] + L[j + 1:]
+        cost += j - i + 1
+
+    print(f"Case #{t}: {cost}")
